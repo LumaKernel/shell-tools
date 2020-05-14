cb07b803b1997f5c4ea25ccaced806dd247abadd
Luma <markermails@gmail.com>
2020-05-14 20:47:22 +0900

    fix around escaping

--- ag-replace.sh
+++ ag-replace.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+function SedRegexSourceEscape() {
+  local STR="$1"
+  STR="${STR//\//\\\/}"
+  echo -n "$STR"
+}
+
+function RegexTargetEscape() {
+  local STR="$1"
+  STR="${STR//\\/\\\\}"
+  STR="${STR//\//\\\/}"
+  echo -n "$STR"
+}
+
 FOUND="$(ag -0 -l -- "$1" | xargs -0 echo)" \
-  && echo "$FOUND" | xargs sed -i.bak -e "s/$1/$2/g"
+  && echo "$FOUND" | xargs sed -i.bak -e "s/$(SedRegexSourceEscape $1)/$(RegexTargetEscape $2)/g"
 
